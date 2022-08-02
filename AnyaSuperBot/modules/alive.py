7b35c5b82165ae418e26c3a276510eb287b5c760
Cremented <110434131+Cremented@users.noreply.github.com>
2022-08-01 21:25:11 -0700

    Update alive.py

--- AnyaSuperBot/modules/alive.py
+++ AnyaSuperBot/modules/alive.py
@@ -5,13 +5,13 @@ import re, os
 from AnyaSuperBot.events import register
 from AnyaSuperBot import telethn as tbot
 from AnyaSuperBot import telethn as tgbot
-PHOTO = "https://telegra.ph/file/d9c46e3db8952c020256b.jpg"
+PHOTO = "https://telegra.ph/file/aec92f8fb1049a82a8fc9.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
-  Saber = f"**剣 Hii {event.sender.first_name} I'm Saber Sword** \n\n"
-  Saber += "**剣 I'm Property OF @AogiriNetwork**\n\n"
-  Saber += "**剣 Anya Forger 剣: Sword Master Version **\n\n"
-  Saber += "**剣 Creator:** [U N K N O W N](t.me/XTheAnonymous)\n\n"
-  Saber += "**剣 python-Telegram-Bot: 13.11**\n\n"
-  BUTTON = [[Button.url("Support", "https://t.me/NexusXSupport"), Button.url("Updates", "https://t.me/AogiriNetwork")]]
+  Saber = f"**パワー Hii {event.sender.first_name} I'm General Esdeath** \n\n"
+  Saber += "**パワー I'm Property OF @AztecSupport**\n\n"
+  Saber += "**パワー Esdeath 剣: Version **\n\n"
+  Saber += "**パワー Creator:** [U N K N O W N](t.me/XTheAnonymous)\n\n"
+  Saber += "**パワー python-Telegram-Bot: 13.11**\n\n"
+  BUTTON = [[Button.url("Support", "https://t.me/AztecSupport"), Button.url("Updates", "https://t.me/AogiriNetwork")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=Saber,  buttons=BUTTON)
